4e802537d601faa836f1c83a3132150e2d506430
maciej <roszkowskimaiciek@gmail.com>
2018-11-04 22:30:27 +0100

    Add project files.

---
+++ sklepik/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sklepik.Startup))]
+namespace sklepik
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

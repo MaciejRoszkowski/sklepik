9d6be6f69f5f91fd03b06848ba0efa1457295811
maciej <roszkowskimaiciek@gmail.com>
2018-11-14 22:45:38 +0100

    crud done

--- sklepik/Controllers/ProductController.cs
+++ sklepik/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using sklepik.Models;
@@ -14,13 +16,23 @@ namespace sklepik.Controllers
         // GET: Product
         public ActionResult Index()
         {
-            return View();
+
+            return View(_db.Product.ToList());
         }
 
         // GET: Product/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(int? id)
         {
-            return View();
+            if(id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
         }
 
         // GET: Product/Create
@@ -31,40 +43,37 @@ namespace sklepik.Controllers
 
         // POST: Product/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Product product)
         {
-            try
+            if(ModelState.IsValid)
             {
-                // TODO: Add insert logic here
-
+                _db.Product.Add(product);
+                _db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            catch
-            {
-                return View();
-            }
+            return View(product);
         }
 
         // GET: Product/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var model = _db.Product.Find(id);
+            return View(model);
         }
 
-        // POST: Product/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if(ModelState.IsValid)
             {
-                return View();
+                _db.Entry(product).State=EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+
             }
+            return View(product);
         }
 
         // GET: Product/Delete/5
--- sklepik/Models/IdentityModels.cs
+++ sklepik/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace sklepik.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("baza", throwIfV1Schema: false)
         {
         }
 

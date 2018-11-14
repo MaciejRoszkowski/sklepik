3c0a47be473bba3c46486aadb073e14060314b43
maciej <roszkowskimaiciek@gmail.com>
2018-11-14 18:11:57 +0100

    bazka dodana

---
+++ sklepik/Models/ShopDB.cs
@@ -0,0 +1,21 @@
+namespace sklepik.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class ShopDB : DbContext
+    {
+        public ShopDB()
+            : base("name=baza")
+        {
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+        public DbSet<Product> Product { get; set; }
+    }
+}
